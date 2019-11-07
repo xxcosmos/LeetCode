fa89636d54787d4b5c3c1bc40a9482e90e85fee3
xxcosmos <xandcosmos@gmail.com>
2019-11-07 16:48:03 +0800

    merge two binary trees

---
+++ src/LeetCode/MergeTwoBinaryTrees/Solution.java
@@ -0,0 +1,23 @@
+package LeetCode.MergeTwoBinaryTrees;
+
+/**
+ * @author xandcosmos@gmail.com
+ * @date 2019-11-07 16:31
+ */
+public class Solution {
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+
+        if (t1 == null && t2 == null) {
+            return null;
+        } else if (t1 != null && t2 == null) {
+            return t1;
+        } else if (t1 == null && t2 != null) {
+            return t2;
+        }
+        TreeNode currentNode = new TreeNode(t1.val + t2.val);
+        currentNode.left = mergeTrees(t1.left, t2.left);
+        currentNode.right = mergeTrees(t1.right, t2.right);
+        return currentNode;
+
+    }
+}
