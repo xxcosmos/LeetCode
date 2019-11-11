7f191f04fdfc9030ce61fe81690419a30d64f4d9
xxcosmos <xandcosmos@gmail.com>
2019-11-11 16:06:07 +0800

    Make ListNode to be a public class

--- src/LeetCode/AddTwoNumbers/Solution.java
+++ src/LeetCode/AddTwoNumbers/Solution.java
@@ -1,5 +1,8 @@
 package LeetCode.AddTwoNumbers;
 
+
+import LeetCode.ListNode;
+
 public class Solution {
 
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
@@ -14,10 +17,12 @@ public class Solution {
             carry = sum / 10;
             curr.next = new ListNode(sum % 10);
             curr = curr.next;
-            if (l1 != null)
+            if (l1 != null) {
                 l1 = l1.next;
-            if (l2 != null)
+            }
+            if (l2 != null) {
                 l2 = l2.next;
+            }
         }
         if (carry != 0) {
             curr.next = new ListNode(carry);
